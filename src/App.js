caaf2c08e9754b5c063220481bf37e2907fef4ff
Zach Wolek <zach.wolek@gmail.com>
2024-07-02 12:52:56 -0500

    Feat: Creates a Card feature

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import { useState } from 'react';
 import './App.css';
+import Ideas from './Ideas';
 
 function App() {
+  const dummyIdeas = [
+    { id: 1, title: 'Prank Travis', description: 'Stick googly eyes on all his stuff' },
+    { id: 2, title: 'Make a secret password app', description: 'So you and your rideshare driver can both know neither one of you is lying' },
+    { id: 3, title: 'Learn a martial art', description: 'To exact vengeance upon my enemies' },
+]
+  const [ideas, setIdeas] = useState(dummyIdeas)
+
   return (
-   <h1>IdeaBox</h1>
+    <main className='App'>
+      <h1>IdeaBox</h1>
+      <p>Hi!</p>
+      <Ideas name='Travis'/> 
+    </main>
   );
 }
 
---
+++ src/Ideas.js
@@ -0,0 +1,9 @@
+import './Ideas.css'
+
+function Ideas({name}){
+    return (
+        <h2>Hello, Mr. {name}</h2>
+    )
+}
+
+export default Ideas;
